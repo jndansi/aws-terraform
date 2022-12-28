2c89668043d430a81f076e71f8f1699a8c6405da
jndansi <113783671+jndansi@users.noreply.github.com>
2022-12-28 14:01:34 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "name" {
+    default = "project-server1"
+    type = string
+    description = "The name of the aws compute instance"
+}
+
+
+variable "type" {
+    default = "t2.micro"
+    type = string
+    description = "The instance type"
+  
+}
+
+variable "key-pair" {
+    default = "my-key-pair"
+    type = string
+    description = "key pair name"
+  
+}
